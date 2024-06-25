4fc6085882b2e4afb4cbfdbff8515eead0239c63
Olmos2121 <tomyolmos1@hotmail.com>
2024-06-25 12:40:22 -0300

    HomeView completada
    - Solo faltaria agregar los links que te dirigen a las diferentes vistas:
    - ensayos clinicos
    - Historia, politicas y derechos
    - Areas de investigacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header.jsx';
 import { Footer } from './components/Footer.jsx';
-import Home from './views/Home.jsx';
+import { Home } from './views/Home.jsx';
 import AboutUs from './views/AboutUs.jsx';
 import PrivacyPolicy from './views/PrivacyPolicy.jsx';
 import Register from './views/Register.jsx';
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import userExp from '../assets/images/user_experience.png';
+import trialImg from '../assets/images/trial_img.png'
 import '../styles/Home.css';
 
-const Home = () => {
+export const Home = () => {
     return (
-        <div className='home'>
-            <h1>Bienvenidos a TrialMatch</h1>
-            <p>El servicio es excelente, rápido, barato y efectivo...</p>
-            {/* Resto del contenido de la página de inicio */}
-        </div>
+        <>
+            <div className="home">
+                <div className='links-container'>
+                    <a className='link-block' href="">
+                        <h3>Ensayos clinicos</h3>
+                    </a>
+                    <a className='link-block' href="">
+                        <h3>Historia, politicas y derecho</h3>
+                    </a>
+                    <a className='link-block' href="">
+                        <h3>Areas de investigacion</h3>
+                    </a>
+                </div>
+                <div className='coments-block'>
+                    <div className='coment'>
+                        <img src={userExp} alt="Imagen de usuario" />
+                    </div>
+                    <div className='coment'>
+                        <img src={trialImg} alt="Imagen de ensayo clinico" />
+                    </div>
+                </div>
+                <div className='coments-foot'>
+                    <div className='footer-coment'>
+                        <h2><b>Se parte de la red de servicios para ensayos clinicos mas grande del pais.</b></h2>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
-
-export default Home;
